63962c098e61ef0974ce9dd96728c9872916ccbe
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-11-22 18:25:24 +1100

    Tidying up the style (indentation)

--- R/add-n-prop-miss.R
+++ R/add-n-prop-miss.R
@@ -13,6 +13,7 @@
 #'   the label if left blank, otherwise will add "_vars" to distinguish that it
 #'   has not been used on all of the variables.
 #' @param label character default is "n_miss".
+#' @param .parallel logical (or condition) for making parallisation
 #'
 #' @return a dataframe
 #'
@@ -27,16 +28,20 @@
 #'
 #' @importFrom dplyr select
 #' @importFrom rlang quos
-add_n_miss <- function(data, ..., label = "n_miss", .parallel = nrow(data) > 10000){
+add_n_miss <- function(data,
+                       ...,
+                       label = "n_miss",
+                       .parallel = nrow(data) > 10000){
+
   if( missing(...) ){
     data[[paste0(label, "_all")]] <- count_row_na_cpp(data, .parallel )
-  } else {
-    quo_vars <- quos(...)
-    selected_data <- select(data, !!!quo_vars)
-    data[[paste0(label, "_vars")]] <- count_row_na_cpp(selected_data, .parallel)
-  }
+    } else {
+      quo_vars <- quos(...)
+      selected_data <- select(data, !!!quo_vars)
+      data[[paste0(label, "_vars")]] <- count_row_na_cpp(selected_data, .parallel)
+      }
   data
-}
+  }
 
 #' Add column containing proportion of missing data values
 #'
@@ -80,13 +85,17 @@ add_n_miss <- function(data, ..., label = "n_miss", .parallel = nrow(data) > 100
 #' prp(type = 4,
 #'     extra = 101,
 #'     prefix = "prop_miss = ")
-add_prop_miss <- function(data, ..., label = "prop_miss", .parallel = nrow(data) > 10000){
+add_prop_miss <- function(data,
+                          ...,
+                          label = "prop_miss",
+                          .parallel = nrow(data) > 10000){
+
   if( missing(...) ){
     data[[paste0(label, "_all")]] <- prop_row_na_cpp(data, .parallel)
-  } else {
-    quo_vars <- quos(...)
-    selected_data <- select(data, !!!quo_vars)
-    data[[paste0(label, "_vars")]] <- prop_row_na_cpp(selected_data, .parallel)
-  }
+    } else {
+      quo_vars <- quos(...)
+      selected_data <- select(data, !!!quo_vars)
+      data[[paste0(label, "_vars")]] <- prop_row_na_cpp(selected_data, .parallel)
+      }
   data
-}
+  }
